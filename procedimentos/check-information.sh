4e932a22317e368864d4c764036bf13dd0bec793
Thiago Marques <thiagomarquesdums@gmail.com>
2019-02-24 22:22:50 +0000

    Corrigindo variaveis de comando

--- procedimentos/check-information.sh
+++ procedimentos/check-information.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+PROCESSOS=$((`ps aux | wc -l` - 1))
+#PROCESSOS=`ps aux | wc -l`
+UPTIME=`uptime`
+APACHEVERSION=`httpd -v | grep "Server version"`
+UPTIMEAPACHE=`service httpd fullstatus | grep "Server uptime"`
 
-PROCESSOS="$(ps aux | wc -l)"
-UPTIME="$(uptime)"
-APACHEVERSION="$(httpd -v | grep "Server version")"
-UPTIMEAPACHE="(service httpd fullstatus | grep "Server uptime")"
 echo "========================================================"
-echo -e             "Informações da Maquina"                  "
+echo -e             "Informações da Maquina"                  
 echo "========================================================"
 
-echo -e "\nQuantidade de processos :$(("$PROCESSOS" - "1"))"
-echo "UPTIME "$UPTIME""
-echo -e "\nUptime APACHE "$UPTIMEAPACHE""
-echo -e "\nVersão APACHE "$APACHEVERSION""
+echo -e "\nQuantidade de processos: $PROCESSOS"
+echo "UPTIME: $UPTIME"
+echo -e "\nUptime APACHE: $UPTIMEAPACHE"
+echo -e "\nVersão APACHE: $APACHEVERSION"
