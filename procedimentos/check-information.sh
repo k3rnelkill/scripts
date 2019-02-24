8950d46c25c08c9100944e550cc833b5a098956d
Thiago Marques <thiagomarquesdums@gmail.com>
2019-02-24 22:50:34 +0000

    adcionando cores ao script

--- procedimentos/check-information.sh
+++ procedimentos/check-information.sh
@@ -1,19 +1,40 @@
+#!/bin/bash
+
+#################################################################
+#                                                               #
+# Nome: check-information.sh                                    #
+#                                                               #
+# Autor: Thiago Marques (thiagomarquesdums@gmail.com)           #
+# Data: 24/02/19                                                #
+#                                                               #
+# Descrição: Faz checagem de serviços da maquina, processo      #
+# uptimes e etc.						#
+#                                                               #
+# USO: ./check-information.sh                                   #
+#                                                               #
+#################################################################
+
 PROCESSOS=$((`ps aux | wc -l` - 1))
-#PROCESSOS=`ps aux | wc -l`
 UPTIME=`uptime`
 APACHEVERSION=`httpd -v | grep "Server version"`
 UPTIMEAPACHE=`service httpd fullstatus | grep "Server uptime"`
 APACHEPROC=`service httpd fullstatus | grep "currently"`
 PROCOCIOSO=`service httpd fullstatus | grep "idle workers"`
 
-echo "========================================================"
+#DEFININDO CORES
+corPadrao="\033[0m"
+vermelho="\033[0;31m"
+branco="\033[1;37m"
+amarelo="\033[1;33m"
+
+echo "$vermelho========================================================"
 echo "              Informações da Maquina                    "                  
-echo "========================================================"
+echo "========================================================$corPadrao"
 
 echo -e "\nQuantidade de processos: $PROCESSOS"
 echo "UPTIME e LOAD: $UPTIME"
 
-echo -e "\n==================================================="
+echo -e "\n====================================================="
 echo    "           Informações sobre o APACHE                "
 echo    "====================================================="
 
