1f9d419aae58d936c11de3797716b960cfd3a713
Thiago Marques <thiagomarquesdums@gmail.com>
2019-02-24 22:41:25 +0000

    adcionando verificações do apache

--- procedimentos/check-information.sh
+++ procedimentos/check-information.sh
@@ -3,12 +3,21 @@ PROCESSOS=$((`ps aux | wc -l` - 1))
 UPTIME=`uptime`
 APACHEVERSION=`httpd -v | grep "Server version"`
 UPTIMEAPACHE=`service httpd fullstatus | grep "Server uptime"`
+APACHEPROC=`service httpd fullstatus | grep "currently"`
+PROCOCIOSO=`service httpd fullstatus | grep "idle workers"`
 
 echo "========================================================"
-echo -e             "Informações da Maquina"                  
+echo "              Informações da Maquina                    "                  
 echo "========================================================"
 
 echo -e "\nQuantidade de processos: $PROCESSOS"
-echo "UPTIME: $UPTIME"
+echo "UPTIME e LOAD: $UPTIME"
+
+echo -e "\n==================================================="
+echo    "           Informações sobre o APACHE                "
+echo    "====================================================="
+
 echo -e "\nUptime APACHE: $UPTIMEAPACHE"
 echo -e "\nVersão APACHE: $APACHEVERSION"
+echo -e "\nRequisições sendo processadas $APACHEPROC"
+echo -e "\nProcessos ociosos $PROCOCIOSO"
