f8a804812c0ab50402289b1e2dbe8b770cf4b42b
Thiago Marques <thiagomarquesdums@gmail.com>
2019-03-06 14:00:04 +0000

    Script para migrar e-mail com imapsync

--- procedimentos/migra-email.sh
+++ procedimentos/migra-email.sh
@@ -19,24 +19,22 @@
 
 DATAHORA=`date "+%d-%m-%Y %H:%M:%S"`
 LOG="/var/log/migra-email.log"
-#read -p "Informe o servidor de ORIGEM: " SRVORIGEM
-#read -p "Informe o servidor de DESTINO: " SRVDESTINO
-SRVORIGEM=ews2.datatop.com.br
-SRVDESTINO=ewr5.datatop.com.br
+read -p "Informe o servidor de ORIGEM: " SRVORIGEM
+read -p "Informe o servidor de DESTINO: " SRVDESTINO
 
 
 for LINHA in $(cat /root/contas.txt)  
 do	
 	CONTA=`echo $LINHA | awk -F, '{print $1}'`
 	CONTA2=`echo $LINHA | awk -F, '{print $1}'`
-	#SENHA1=`echo $LINHA | awk -F, '{print $2}'`
-	#SENHA2=`echo $LINHA | awk -F, '{print $3}'`
+	SENHA1=`echo $LINHA | awk -F, '{print $2}'`
+	SENHA2=`echo $LINHA | awk -F, '{print $3}'`
 
 	#echo -e "\n Iniciando sincronia da conta $CONTA -> $DATAHORA" >> $LOG
 
 	#echo "$CONTA""-"$SENHA1"-""$CONTA""-"$SENHA2
 
- 	imapsync --host1 $SRVORIGEM --user1 teste2@commitlinux.com.br  --password1 '0YIKynhqWWyY' --host2 $SRVDESTINO --user2 teste2@commitlinux.com.br --password2 '0YIKynhqWWyY'
+ 	imapsync --host1 $SRVORIGEM --user1 $CONTA1 --password1 $SENHA1 --host2 $SRVDESTINO --user2 $CONTA2 --password2 $SENHA2
 	#echo $? >> $LOG
 
  	#echo -e "\nFinalizado sincronia da conta $CONTA -> $DATAHORA" >> $LOG
