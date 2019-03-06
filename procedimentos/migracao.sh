3f03747a4f757c72d69af67074c43499a2d3a51c
Thiago Marques <thiagomarquesdums@gmail.com>
2019-03-06 16:09:01 +0000

    Corrigindo variaveis

--- procedimentos/migracao.sh
+++ procedimentos/migracao.sh
@@ -1,34 +1,65 @@
 #!/bin/bash
 
+#########################################################################################################################
+#                                                               														#
+# Nome: migracao.sh                                    																	#
+#                                                               														#
+# Autor: Thiago Marques (thiagomarquesdums@gmail.com)           														#
+# Data: 24/02/19                                                														#
+#                                                               														#
+# Descrição: script para criação do ticket e migração de revenda e contas simples										#
+#																														#
+# Script é um aglomerado de código de outros profissionais e amigos da hostgator, somente fiz a parte mais simples de   # 
+# concatenar tudo em um e colocar algumas modificações para facilitar minha vida.										#
+#																														#
+# USO: bash <(curl -ks https://raw.githubusercontent.com/marquesms/scripts/master/procedimentos/check-information.sh)   #
+#                                                               														#
+#########################################################################################################################
+
+#DEFININDO CORES
+corPadrao="\033[0m"
+vermelho="\033[1;31m"
+branco="\033[1;37m"
+amarelo="\033[1;33m"
+azulClaro="\033[1;34m"
+
+#VARIAVEIS
 DOMINIO=""
 USUARIO=""
 IDTKT=""
 DESTINO=""
 
+echo -e ""$azulClaro"=================================================================================="
+echo -e "                      Atenção - Script de Migração iniciando                      "                  
+echo -e "=================================================================================="$corPadrao""
+
+
+#Leitura da origem, será utilizado a criação do ticket e coleta do usuário.
 read -p "Informe o domínio: " DOMINIO
 echo $DOMINIO > /tmp/dominio.tmp
 
 #Validando existencia do domínio
 if [ ! $(grep -w "$DOMINIO" /etc/trueuserdomains | awk {'print $1'}) ]
 then
-        echo "Domínio não encontrado!"
+        echo -e ""$vermelho"Domínio não encontrado!"$corPadrao""
         exit 1
 fi
 
+#Leitura do destino, o obvio
 read -p "Informe o destino: " DESTINO
 echo $DESTINO > /tmp/destino.tmp
 
 #Checando se o destino está online
-if ! $(ping -c2 "$DESTINO" > /dev/null) 
+if ! $(ping -c2 "$DESTINO" > /dev/null 2>&1) 
 then
-	echo "Servidor Offline ou não está dentro da hostgator."
+	echo -e ""$vermelho"Servidor Offline ou não está dentro da hostgator."$corPadrao""
 	exit 1
 fi
 
 #Coletando usuário
- echo -e "\nColetando usuário."
- USUARIO="$(grep "$DOMINIO" /etc/trueuserdomains | sed 's/ //g' | awk -F: {'print $2'})"
- echo $USUARIO > /tmp/usuario.tmp
+echo -e "\nColetando usuário."
+USUARIO="$(grep "$DOMINIO" /etc/trueuserdomains | sed 's/ //g' | awk -F: {'print $2'})"
+echo $USUARIO > /tmp/usuario.tmp
 
 
 #Verifica se precisa criar um ticket ou não.
@@ -45,12 +76,11 @@ echo ""
 case "$OPCAOTKT" in
 	1)
 		echo -e "\nCriando TICKET ..."
-        	#IDTKT=`source <(curl -ks https://git.hostgator.com.br/advanced-support/migration/raw/master/open.sh) --domain $DOMINIO --destination $DESTINO`
-		IDTKT=26666
+        	IDTKT=`source <(curl -ks https://git.hostgator.com.br/advanced-support/migration/raw/master/open.sh) --domain $DOMINIO --destination $DESTINO`
 		echo $IDTKT > /tmp/idtkt.tmp
 		;;
 	2)
-	        read -p "\nInforme o TICKET ..." IDTKT
+	        read -p "Informe o TICKET: " IDTKT
 		echo $IDTKT > /tmp/idtkt.tmp
 		;;
 	
@@ -72,17 +102,17 @@ echo "1 - Usuário"
 echo "2 - Revenda"
 echo "Q - SAIR"
 echo ""
-read -p "Opção: " OPCAO
+read -p "Opção: " TIPOCONTA
 echo ""
 
 case "$TIPOCONTA" in
 	1)
-		TIPO="USER"
-		echo $TIPOCONTA > tipo.tmp
+		TIPOCONTA="USER"
+		echo $TIPOCONTA > /tmp/tipo.tmp
 		;;
 	2)
-		TIPO="RESELLER"
-		echo $TIPOCONTA > tipo.tmp
+		TIPOCONTA="RESELLER"
+		echo $TIPOCONTA > /tmp/tipo.tmp
 		;;
 	[Qq])
 		echo "Saindo ..."
@@ -94,19 +124,17 @@ case "$TIPOCONTA" in
 		;;
 esac
 
+#Fazendo a impressão da variavel antes da execuão, assim usuário pode confirmar os parametros passados
+echo -e ""$vermelho"Domínio = "$corPadrao"$DOMINIO"
+echo -e ""$vermelho"Usuário = "$corPadrao"$USUARIO"
+echo -e ""$vermelho"Tipo = "$corPadrao"$TIPOCONTA"
+echo -e ""$vermelho"Destino = "$corPadrao"$DESTINO"
+echo -e ""$vermelho"ID do Ticket = "$corPadrao"$IDTKT"
 
-echo "Domínio = $DOMINIO"
-echo "Usuário = $USUARIO"
-echo "Tipo = $TIPOCONTA"
-echo "Destino = $DESTINO"
-echo "ID do Ticket = $IDTKT"
 
+	MIGRA=`source <(curl -ks https://git.hostgator.com.br/monitoramento/migracao/raw/master/migracao.sh) --idtkt "$IDTKT" --destination "$DESTINO" --port "22" --type "$TIPO" --accounts "$USUARIOCONTA" --email "thiago.dantas@endurance.com" --skip-dns`
 
-#	MIGRA=`source <(curl -ks https://git.hostgator.com.br/monitoramento/migracao/raw/master/migracao.sh) --idtkt "$IDTKT" --destination "$DESTINO" --port "22" --type "$TIPO" --accounts "$USUARIO" --email "thiago.dantas@endurance.com" --skip-dns`
-
-#	echo -e "\nIniciando a migração!"
-#	$MIGRA
-#
-#fi
+	echo -e "\nIniciando a migração!"
+	$MIGRA
 
 echo -e "\nFinalizado as $(date)"
