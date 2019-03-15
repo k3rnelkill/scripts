65e1e42bc0595312b42bde5b76166619d5874ab0
Thiago Marques <thiagomarquesdums@gmail.com>
2019-03-15 04:25:57 -0300

    Update install_imapsync.sh

--- procedimentos/install_imapsync.sh
+++ procedimentos/install_imapsync.sh
@@ -2,16 +2,16 @@
 
 ##########################################################################
 #                                                                        #
-# Nome: install_imapsync.sh		                                         #
+# Nome: install_imapsync.sh		                                           #
 #                                                                        #
 # Autor: Thiago Marques (thiagomarquesdums@gmail.com)                    #
 # Data: 15/03/2019                                                       #
 #                                                                        #
 # Descrição: Faz a instalação do imapsync em diferentes sistemas         #
 # operacionais links, script essencial para utilizar o migra-email.sh    #
-#								                                         #
-# Homologado: Em desenvolvimento				                         #
-#                 					                                     #
+#								                                                         #
+# Homologado: Em desenvolvimento				                                 #
+#                 					                                             #
 #                                                                        #
 # USO: ./install_imapsync.sh ou bash <(curl -ks )                        #
 #                                                                        #
