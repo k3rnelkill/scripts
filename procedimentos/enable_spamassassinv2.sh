21fac3504e86a8d4e263b55ab4db40f06c70cde4
Thiago Marques <thiagomarquesdums@gmail.com>
2019-04-02 08:45:26 +0000

    Versão 2 utiliza as proprias API do cPanel para fazer a ativação e desativação do serviço

--- procedimentos/enable_spamassassinv2.sh
+++ procedimentos/enable_spamassassinv2.sh
@@ -1,61 +1,69 @@
 #!/bin/bash
 
+#########################################################################################################################################
+#                                                                                                                                       #
+# Nome: enable_spamassassinv2.sh                                                                                                        #
+#                                                                                                                                       #
+# Autor: Thiago Marques (thiagomarquesdums@gmail.com)                                                                                   #
+# Data: 02/04/19                                                                                                                        #
+#                                                                                                                                       #
+# Descrição: Faz ativação do apachespamassassin e criação do filtro com algumas regras funcionas                                        #
+#                                                                                                                                       #
+# USO: ./enable_spamassassinv2.sh ou                                                                                                    #
+# bash <( -ks https://raw.githubusercontent.com/marquesms/scripts/master/procedimentos/enable_spamassassinv2.sh                         #
+#                                                                                                                                       #
+#########################################################################################################################################
+
 USUARIO=`pwd | awk -F/ {'print $3'}`
 HOMEUSER=`grep "$USUARIO" /etc/passwd | awk -F: {'print $6'}`
-DIR="$HOMEUSER"/.spamassassin""
 
 echo $USUARIO
 echo $HOMEUSER
 
 if [ -f $HOMEUSER"/.spamassassinenable" ]
 then
-	echo -e "Executando o IF"
-	echo -e "\nJá existe um Spam Filter ativo"
-	echo ""
-	echo -e "\nDeseja sobrescrever a configuração?"
-	echo ""
-	echo "1 - SIM"
-	echo "2 - NÂO"
-	echo ""
-	read -p "Opcao: " OPCAO
-	echo ""
+        echo -e "Executando o IF"
+        echo -e "\nJá existe um Spam Filter ativo"
+        echo ""
+        echo -e "\nDeseja sobrescrever a configuração?"
+        echo ""
+        echo "1 - SIM"
+        echo "2 - NÂO"
+        echo ""
+        read -p "Opcao: " OPCAO
+        echo ""
 
-	case "$OPCAO" in
-		1)	
-			if [ ! -d $DIR ]
-        		then
-                		echo "Criando o diretório $DIR ..."
-                		mkdir -p $DIR
-                		echo "required_score 5" > $DIR"/user_prefs"
-			else
-				echo "required_score 5" > $DIR"/user_prefs"	
-			fi
-			;;
-		2) 
-			echo "Saindo ..."
-			exit
-			;;	
-		*)
-			echo "Opção selecionada invalida"
-			exit 2
-			;;
-	esac	
+        case "$OPCAO" in
+                1)          
+                        if [ ! -d $DIR ]
+                        then
+                                echo "Refazendo ativação padrão"
+                                sleep 1
+                                uapi --user="$USUARIO" Email disable_spam_assassin
+                                echo "Score Spam 5.0"
+                                sleep 1
+                                uapi --user="$USUARIO" Email enable_spam_assassin 
+                                uapi --user="$USUARIO" SpamAssassin update_user_preference preference=score value-0="ACT_NOW_CAPS 5.0"
+                        fi  
+                        ;;  
+                2)  
+                        echo "Saindo ..."
+                        exit
+                        ;;          
+                *)  
+                        echo "Opção selecionada invalida"
+                        exit 2
+                        ;;  
+        esac    
 
 else
-	echo -e "Executando o ELSE\n"
-	if [ ! -d $DIR ]
-	then
-           	echo "Criando o diretório $DIR ..."
-            	mkdir -p $DIR
-           	#touch $DIR"/user_prefs"
-		touch $HOMEUSER"/.spamassassinenable"
-           	echo "required_score 5" > $DIR"/user_prefs"
-	fi
+    
+        echo "Ativando SPAMASSASSIN do usuário $USUARIO ..."
+        sleep 1
+        uapi --user="$USUARIO" Email enable_spam_assassin
+        echo "Score Spam 5.0"
+        sleep 1
+        uapi --user="$USUARIO" SpamAssassin update_user_preference preference=score value-0="ACT_NOW_CAPS 5.0"
+    
 
-	if [ ! -f $DIR"/user_prefs" ]
-	then
-		echo "Criando arquivo user_prefs"
-		#touch $DIR"/user_prefs"
-		echo "required_score 5" > $DIR"/user_prefs"
-	fi
 fi
