4f47fa8a3885ce9df3bcd0d1c484ef23719b47bc
root <root@ENDUSUP2127.latam-eig.com>
2019-04-06 03:40:58 +0000

    Habilitando case para habilitar filtros sem rescrever spamassassin que estiver habilitado

--- procedimentos/enable_spamassassinv2.sh
+++ procedimentos/enable_spamassassinv2.sh
@@ -18,13 +18,13 @@ USUARIO=`pwd | awk -F/ {'print $3'}`
 HOMEUSER=`grep "$USUARIO" /etc/passwd | awk -F: {'print $6'}`
 DIR="$HOMEUSER"/.spamassassin""
 DATA=`date +%Y%m%d"_"%H%M`
+FILTRO=`wget https://raw.githubusercontent.com/marquesms/scripts/master/procedimentos/filter.txt -O "$HOMEUSER/filter.txt"`
+CACHEFILTRO=`wget https://raw.githubusercontent.com/marquesms/scripts/master/procedimentos/filter.cache.txt -O "$HOMEUSER/filter.cache.txt"`
 
 echo $USUARIO
 echo $HOMEUSER
 
 
-wget https://raw.githubusercontent.com/marquesms/scripts/master/procedimentos/filter.txt -O "$HOMEUSER/filter.txt"
-wget https://raw.githubusercontent.com/marquesms/scripts/master/procedimentos/filter.cache.txt -O "$HOMEUSER/filter.cache.txt"
 
 if [ -f $HOMEUSER"/.spamassassinenable" ]
 then
@@ -33,35 +33,51 @@ then
 	echo ""
 	echo -e "\nDeseja sobrescrever a configuração?"
 	echo ""
-	echo "1 - SIM"
-	echo "2 - NÂO"
+	echo "1 - SIM Sobrescrever"
+	echo "2 - Aplicar somente o Filtros de SPAM"
+	echo ""
+	echo -e "3 - SAIR"
 	echo ""
 	read -p "Opcao: " OPCAO
 	echo ""
 
 	case "$OPCAO" in
 		1)          
-				echo "Refazendo ativação padrão"
-				sleep 1
-				uapi --user="$USUARIO" Email disable_spam_assassin
-				echo "Score Spam 5.0"
-				sleep 1
-				uapi --user="$USUARIO" Email enable_spam_assassin 
-				uapi --user="$USUARIO" SpamAssassin update_user_preference preference=score value-0="ACT_NOW_CAPS 5.0"
-				echo -e "Habilitando auto Delete"
-				sleep 1
-				cpapi1 --user="$USUARIO" Email enable_spam_autodelete
-				echo -e "Efetuando backup do vFilter"
-				for DOMINIOS in $(/bin/cat /etc/userdomains | grep "$USUARIO" | awk -F: {'print $1'}); do /bin/cp -pv /etc/vfilters/"$DOMINIOS" "$HOMEUSER"/"$DATA"_vfilter_"$DOMINIOS"; done
-				sleep 1
-				echo -e "Habilitando o Spam Filter"
-				for DOMINIOS in $(/bin/cat /etc/userdomains | grep "$USUARIO" | awk -F: {'print $1'}); do /bin/cat filter.txt > /etc/vfilters/"$DOMINIOS"; done
-				/bin/cat "$HOMEUSER"/filter.cache.txt > "$HOMEUSER"/.cpanel/filter.cache
+			$FIlTRO
+			$CACHEFILTRO
+			echo "Refazendo ativação padrão"
+			sleep 1
+			uapi --user="$USUARIO" Email disable_spam_assassin
+			echo "Score Spam 5.0"
+			sleep 1
+			uapi --user="$USUARIO" Email enable_spam_assassin 
+			uapi --user="$USUARIO" SpamAssassin update_user_preference preference=score value-0="ACT_NOW_CAPS 5.0"
+			echo -e "Habilitando auto Delete"
+			sleep 1
+			cpapi1 --user="$USUARIO" Email enable_spam_autodelete
+			echo -e "Efetuando backup do vFilter"
+			for DOMINIOS in $(/bin/cat /etc/userdomains | grep "$USUARIO" | awk -F: {'print $1'}); do /bin/cp -pv /etc/vfilters/"$DOMINIOS" "$HOMEUSER"/"$DATA"_vfilter_"$DOMINIOS"; done
+			sleep 1
+			echo -e "Habilitando o Spam Filter"
+			for DOMINIOS in $(/bin/cat /etc/userdomains | grep "$USUARIO" | awk -F: {'print $1'}); do /bin/cat filter.txt > /etc/vfilters/"$DOMINIOS"; done
+			/bin/cat "$HOMEUSER"/filter.cache.txt > "$HOMEUSER"/.cpanel/filter.cache
 			;;  
 		2)  
+			$FIlTRO
+			$CACHEFILTRO
+			echo "Aplicando Filtros"
+			sleep 1		
+			echo -e "Efetuando backup do vFilter"
+			for DOMINIOS in $(/bin/cat /etc/userdomains | grep "$USUARIO" | awk -F: {'print $1'}); do /bin/cp -pv /etc/vfilters/"$DOMINIOS" "$HOMEUSER"/"$DATA"_vfilter_"$DOMINIOS"; done
+			sleep 1
+			echo -e "Habilitando o Spam Filter"
+			for DOMINIOS in $(/bin/cat /etc/userdomains | grep "$USUARIO" | awk -F: {'print $1'}); do /bin/cat filter.txt > /etc/vfilters/"$DOMINIOS"; done
+			/bin/cat "$HOMEUSER"/filter.cache.txt > "$HOMEUSER"/.cpanel/filter.cache
+			;; 
+		3)
 			echo "Saindo ..."
 			exit
-			;;          
+			;;
 		*)  
 			echo "Opção selecionada invalida"
 			exit 2
@@ -69,7 +85,8 @@ then
 	esac    
 
 else
-
+	$FIlTRO
+	$CACHEFILTRO
 	echo "Entrando no else de ativação"
 	echo "Ativando SPAMASSASSIN do usuário "$USUARIO" ..."
 	sleep 1
