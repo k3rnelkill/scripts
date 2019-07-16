fe59874f306f73f873feafe892caa58964f4696a
Thiago Marques <thiagomarquesdums@gmail.com>
2019-07-16 12:57:49 -0300

    update
    update dos comentários para identificação e manutenção futura.

--- procedimentos/alter_password_mail.sh
+++ procedimentos/alter_password_mail.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+##########################################################################################################################################
+#                                                                                                                                        #
+# Name: alter_password_mail.sh                                                                                                           #
+#                                                                                                                                        #
+# Author: Thiago Marques (thiagomarquesdums@gmail.com)                                                                                   #
+# Date: 17/07/19                                                                                                                         #
+#                                                                                                                                        #
+# Description: Used to change the password for the email account.                                                                        #
+#                                                                                                                                        #
+# Use: ./bkp-mysql.sh or bash <(curl -ks https://raw.githubusercontent.com/marquesms/scripts/master/procedimentos/alter_password_mail.sh)#
+#                                                                                                                                        #
+##########################################################################################################################################
+
 #DEFINE COLORS
 corPadrao="\033[0m"
 verde="\033[1;32m"
@@ -7,19 +20,25 @@ vermelho="\033[1;31m"
 branco="\033[1;37m"
 amarelo="\033[1;33m"
 
+#Collect user cPanel
 USERCOLLECT=`pwd | awk -F/ {'print $3'}`
-#PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n1`
-PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n1 > /tmp/passtemp.txt`
+#Collect path cPanel user
 USERDIRECTORY=`grep "$USERCOLLECT" /etc/passwd | cut -d: -f6`
+#Collect Sellect domain
 DOMAIN=`grep "$USERCOLLECT" /etc/trueuserdomains | awk '{print $1}' | sed 's/\://'`
+#Collect e-mail path
 EMAILPATH="$USERDIRECTORY/mail/$DOMAIN/"
+#Collect user domains
 USERDOMAIN=`grep "$USERCOLLECT" /etc/userdomains`
+#Count the amount of domain. 
 COUNTDOMAIN=`grep "$USERCOLLECT" /etc/userdomains | wc -l`
 
+#Show collected information
 echo -e ""$vermelho"Informações coletadas."$corPadrao""
 echo -e ""$vermelho"Usuário: "$amarelo"$USERCOLLECT"$corPadrao""
 echo -e ""$vermelho"Path "$amarelo"$USERDIRECTORY"$corPadrao""
 
+#Identifying the existence of more than one domain
 if [ `echo $COUNTDOMAIN` -gt 1 ]
 then 
 	echo -e ""$vermelho"Domínios: "$corPadrao""
@@ -33,12 +52,19 @@ then
 	echo -e "================================"
 	sleep 3
 	#ls -1 $EMAILPATH | sed 's/\///' | sed 's/^\.*//' | sed '/^$/d'
+	#ls collect e-mail account, loop it will scroll through the list of email accounts collected through ls
 	for CONTAS in $(ls -1 $EMAILPATH | sed 's/\///' | sed 's/^\.*//' | sed '/^$/d')
-       	do 
+       	do
+       	#Create temporary password and send file passtemp
 		/bin/cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n1 > /tmp/passtemp.txt
-		echo -e "\nUsuário: $CONTAS Senha: `cat /tmp/passtemp.txt`;" 
-		/usr/bin/uapi --user="$USERCOLLECT" Email passwd_pop email="$CONTAS" password=`cat /tmp/passtemp.txt` domain="$DOMAIN" 
+		echo -e "================================"
+		#Printing password
+		echo -e "\n"$vermelho"Usuário:"$corPadrao" "$CONTAS" "$vermelho"Senha:"$corPadrao" `cat /tmp/passtemp.txt`" 
+		#
+		/usr/bin/uapi --user="$USERCOLLECT" Email passwd_pop email="$CONTAS" password=`cat /tmp/passtemp.txt` domain="$DOMAIN" 2>&1 /tmp/alter_pass.txt
+		echo -e "================================"
 	done
+#will change the password of the unique account	
 else
 	echo -e "================================"
 	echo -e ""$vermelho"Domínio selecionado: "$amarelo"$DOMAIN""$corPadrao"
@@ -46,5 +72,12 @@ else
 	echo -e "================================" 
 	sleep 3
 	#ls -1 $EMAILPATH | sed 's/\///' | sed 's/^\.*//' | sed '/^$/d'
-	#for CONTAS in $(ls -1 $EMAILPATH | sed 's/\///' | sed 's/^\.*//' | sed '/^$/d'); do uapi --user="$USERCOLLECT" Email passwd_pop email="$CONTAS" password="$PASS" domain="$DOMAIN"; done
+	for CONTAS in $(ls -1 $EMAILPATH | sed 's/\///' | sed 's/^\.*//' | sed '/^$/d')
+       	do 
+		/bin/cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n1 > /tmp/passtemp.txt
+		echo -e "================================"
+		echo -e "\n"$vermelho"Usuário:"$corPadrao" "$CONTAS" "$vermelho"Senha:"$corPadrao" `cat /tmp/passtemp.txt`" 
+		/usr/bin/uapi --user="$USERCOLLECT" Email passwd_pop email="$CONTAS" password=`cat /tmp/passtemp.txt` domain="$DOMAIN" 2>&1 /tmp/alter_pass.txt
+		echo -e "================================"
+	done
 fi
