80392eba669f38584bf4b3171668f0d5d9ca9df5
Thiago Marques <thiagomarquesdums@gmail.com>
2019-07-31 09:36:54 -0300

    Valid DNS and define print colors

--- procedimentos/check-dns-mg.sh
+++ procedimentos/check-dns-mg.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-USUARIO=""
 QTDSUSPEND=`egrep -rli 'internal|latam|migration' /var/cpanel/suspended/ | wc -l`
 SHOWUSERS=`egrep -rli 'internal|latam|migration' /var/cpanel/suspended/ | awk -F"/" {'print $5'}`
 
-if [ $QTDSUSPEND -gt 0 ] 
-then
-	echo -e "Migration located. Check supergator, please"
-	sleep 2
-	echo -e "Migrations completed. Users bellow."
-	echo -e "$SHOWUSERS"
+#COLORS
+defaultColor="\033[0m"
+green="\033[1;32m"
+red="\033[1;31m"
+yellow="\033[1;33m"
 
-	read -p "Informe o usuário : " $USUARIO
-	
-	ORIGIN=$(ui -d "${USUARIO:-VAZIO}" | grep "NS1" | awk '{print $2}')
-	DESTW=$(whois `ui -d "${USUARIO:-VAZIO}" | grep "U. Domain" | awk '{print $3}'` | grep "nserver" | tail -n1 | awk '{print $2}')
-	if [ $ORIGIN != $DESTW ]
-	then
-		echo $ORIGIN
-		echo $DESTW
-		echo -e "DNS alteradas, possibilidade de remoção existente antes do prazo."
-	else
-		echo -e "DNS não alteradas. Remover na data."
-	fi
+if [ $QTDSUSPEND -gt 0 ]
+then
+        echo -e ""$yellow"Migration located. Check supergator, please"$defaultColor""
+        sleep 2
+        echo -e ""$yellow"Migrations completed. Users bellow."$defaultColor""
+        echo -e "$green""\n$SHOWUSERS"$defaultColor""
+
+        read -p "Informe the user :" USUARIO
+
+        DNSORIGIN=$(ui -d "${USUARIO:-VAZIO}" | grep "NS2" | awk '{print $2}')
+        DNSDEST=$(whois `ui -d "${USUARIO:-VAZIO}" | grep "U. Domain" | awk '{print $3}'` | grep "nserver" | tail -n1 | awk '{print $2}')
+        if [ $DNSORIGIN != $DNSDEST ]
+        then
+                echo -e ""$green"\nDNS are already changed to the new server, possibility of removal existing before the deadline."$defaultColor""
+        else
+                echo -e ""$red"\nDNS não alteradas. Remover na data."$defaultColor""
+        fi
 else
-	echo "None user suspended on finished."
+        echo -e ""$green"\nNone user suspended on finished."$defaultColor""
 fi
