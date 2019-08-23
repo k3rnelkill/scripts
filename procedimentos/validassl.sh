a4d5de3368b8630dc13753aed0529b7652b68b3c
Thiago Marques <thiagomarquesdums@gmail.com>
2019-08-23 14:45:20 -0300

    check SSL validity

---
+++ procedimentos/validassl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CURRENTYEAR=`date "+%Y"`
+CURRENTMONTH=`date "+%b"`
+MATURITYEAR=`curl -v https://hostgator.com.br 2>&1 | grep "expire date" | awk '{print $7}'`
+MONTH=`curl -v https://hostgator.com.br 2>&1 | grep "expire date" | awk '{print $4}'`
+
+
+echo "Ano atual $CURRENTYEAR"
+echo "Ano vencimento $MATURITYEAR"
+
+if [ $CURRENTYEAR -lt $MATURITYEAR ]
+then
+	echo "Fora do ano de vencimento de vencimento"
+	echo "Mês = $MONTH"
+else 
+	echo "Certificado vencera este ano."
+	echo "Mês = $MONTH"
+	if [ $MONTH = $CURRENTMONTH ]
+	then
+		echo "Certificado vencerá esse mês"
+	else
+		echo "Fora do mês de vencimento"
+	fi
+fi
