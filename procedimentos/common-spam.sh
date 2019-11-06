5fcd3a0fd0aa225e8b88e3958f6b2772e45698b1
Thiago Marques <thiagomarquesdums@gmail.com>
2019-11-06 13:58:27 -0300

    ...

--- procedimentos/common-spam.sh
+++ procedimentos/common-spam.sh
@@ -19,35 +19,53 @@
 #ALTER VARIABLE DEFAULT IFS
 IFSback=$IFS
 IFS=$'\n'
+LOGFILE="/var/log/exim_mainlog"
 
 #RESET TMP FILE SPAMMER
-echo > /tmp/possible_spammer.txt 
+echo > /tmp/possible_spammer.txt
 echo > /tmp/domain_spammer.txt
 
 #GET COMMON WORDS USED IN SPAM
 /usr/bin/wget https://raw.githubusercontent.com/marquesms/scripts/master/arquivos/common-spam.txt -O /tmp/common-spam.txt
 
 #LOOP THAT WILL CYCLE THROUGH THE EXIM_MAINLOG FILE FOR KEYWORDS IDENTICAL TO THE common.spam.txt FILE
-#USE THIS LINE TO SEARCH ON A SPECIFIC DATE
-#for i in $(cat /tmp/common-spam.txt); do grep -w $i /var/log/exim_mainlog | grep 2019-10-23; done
-for i in $(cat /tmp/common-spam.txt); do grep -w $i /var/log/exim_mainlog | grep `/bin/date +%Y-%m-%d` | grep "<=" | awk -F\@ '{print $2}' | awk '{print $1}' | tee -a /tmp/temp_spammer.txt; done
-#for i in $(cat /tmp/common-spam.txt); do grep -w $i /var/log/exim_mainlog | grep "2019-10-30" | grep "<=" | awk -F\@ '{print $2}' | awk '{print $1}' | tee -a /tmp/temp_spammer.txt; done
+if [ -f /tmp/hour_spam.txt ]
+then
+	for i in $(cat /tmp/common-spam.txt)
+	do 
+		grep -A 1000000 -w "$(cat /tmp/hour_spam.txt)" $LOGFILE | grep -w $i | grep "<=" | awk -F\@ '{print $2}' | awk '{print $1}' 2>&1 >> /tmp/temp_spammer.txt
+	done
+else
+	for i in $(cat /tmp/common-spam.txt)
+	do
+		grep -w $i $LOGFILE | grep $(/bin/date +%Y-%m-%d) | grep "<=" | awk -F\@ '{print $2}' | awk '{print $1}' 2>&1 >> /tmp/temp_spammer.txt
+	done
+fi
 
 #REMOVED DUPLICATE LINES
 cat /tmp/temp_spammer.txt | sort | uniq > /tmp/possible_spammer.txt
 #REMOVED TEMP FILE
 rm -f /tmp/temp_spammer.txt
 
-#for i in $(cat /tmp/domain_spammer.txt); do grep $i /etc/userdomain ; echo $? ; done
 for x in $(cat /tmp/possible_spammer.txt)
 do
-	grep $x /etc/userdomains
-	if [ `echo $?` -eq 0 ]
-	then
-		echo $x >> /tmp/domain_spammer.txt
-	fi
+        grep $x /etc/userdomains
+        if [ `echo $?` -eq 0 ]
+        then
+                echo $x >> /tmp/domain_spammer.txt
+        fi
 done
 
+#CREATE VARIABLE SPAMMER TO ZABBIX
+if [ `cat /tmp/domain_spammer.txt | wc -l` -gt 1 ]
+then
+        echo 1 > /tmp/spammer.txt
+else
+        echo 0 > /tmp/spammer.txt
+fi
+
+#SAVE TIME EXEC - COLLECT LAST EMAIL SEND
+/bin/cat /var/log/exim_mainlog | grep "<=" | tail -n1 | awk -F\. '{print $1}' > /tmp/hour_spam.txt
 
 #RETURN BACKUP IFS
 IFS=$IFSback
