c262073e4de1bab2e63fb56dc2826e54867f558b
Thiago Marques <thiagogardenajj@gmail.com>
2020-01-20 21:14:02 -0300

    deployer 1ª version

--- procedimentos/acctsuspended.sh
+++ procedimentos/acctsuspended.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
-#DEFININDO CORES
-defaultColor="\033[0m"
-verde="\033[1;32m"
+##########################################################################################################################################
+#                                                                                                                                        #
+# Name: acctsysoebded.sh                                                                                                           		 #
+#                                                                                                                                        #
+# Author: Thiago Marques (thiagomarquesdums@gmail.com)                                                                                   #
+# Date: 20/01/20                                                                                                                         #
+#                                                                                                                                        #
+# Description: script finds the server and suspends it.                                                                     			 #
+#                                                                                                                                        #
+# Use: acctsysoebded																										             #
+#                                                                                                                                        #
+##########################################################################################################################################
+
+#DEFINES COLORS
+DEFAULTCOLOR="\033[0m"
+GREEN="\033[1;32m"
 RED="\033[1;31m"
-branco="\033[1;37m"
-amarelo="\033[1;33m"
+YELLOW="\033[1;33m"
 
-read -p "Informe o Domínio.: " DOMINIO
-CLUSTERNAMESERVER="DNS NAMESERVER HERE"
+#VARIABLE USED TO STORE DOMAIN
+read -p "Enter the domain.: " DOMAIN
+#VARIABLE USED TO STORE CPANEL DNS CLUSTER
+CLUSTERNAMESERVER="CLUSTER DNS HERE"
+#SCRIPT PARA SUSPENSÃO
 CMDSUSPEND="/scripts/suspendacct"
-CMDUNSUPEND="/scripts/unsuspendacct"
-CHECKDOMAINIP=$(dig A cpanel.$DOMINIO $CLUSTERNAMESERVER  | grep -w "cpanel.$DOMINIO" | tail -n1 | head -1 | awk '{print $5}')
-COLLECTUSER=$(ssh root@$CHECKDOMAINIP -p22022 grep -w $DOMINIO /etc/trueuserdomains | awk '{print $2}')
+#CHECKDOMAIN IN SERVER
+CHECKDOMAINIP=$(dig A cpanel.$DOMAIN $CLUSTERNAMESERVER  | grep -w "cpanel.$DOMAIN" | tail -n1 | head -1 | awk '{print $5}')
+#COLLECT USER IN CPANEL
+COLLECTUSER=$(ssh root@$CHECKDOMAINIP -p22022 grep -w $DOMAIN /etc/trueuserdomains | awk '{print $2}')
+#COLLECT HOSTNAME
+COLLECTHOSTNAME=$(ssh root@$CHECKDOMAINIP -p22022 uname -n)
 
-echo $DOMINIO
+#TEST VARIABLE
+#echo $DOMAIN
 
+#IF VARIABLE $CHECKDOMAINIP IS NULL, DOMAIN is not found. IN CASE OF NON-NULL VARIABLE. ELSE DEIPT THIS SUSPENSION WILL BE EXECUTED...
 if [ -z $CHECKDOMAINIP ]
 then
-
-	echo "Entrou no if NULO"
-
-	echo -e ""$RED"Variable is empty - Domain not found"$defaultColor""
-	echo -e ""$RED"Domínio não encontrado no pull de DNS"$defaultColor""
+	#echo "Entrou no if NULO"
+	echo -e ""$RED"Variable is empty - $DOMAIN not found"$DEFAULTCOLOR""
+	echo -e ""$RED"$DOMAIN not found in DNS PULL"$DEFAULTCOLOR""
 else
-
-	echo "Entrou no else, dados validos"
-	echo "Domain.: $DOMINIO"
-	echo "IP Server.: $CHECKDOMAINIP"
-	echo "User cPanel.: $COLLECTUSER"
-	
+	#echo "Entrou no else, dados validos"
+	echo -e ""$YELLOW"Domain.:"$DEFAULTCOLOR" $DOMAIN"
+	echo -e ""$YELLOW"IP Server.:"$DEFAULTCOLOR" $CHECKDOMAINIP"
+	echo -e ""$YELLOW"HOSTNAME SERVER:"$DEFAULTCOLOR" $COLLECTHOSTNAME"
+	echo -e ""$YELLOW"User cPanel.:"$DEFAULTCOLOR" $COLLECTUSER"
+	#SUSPENSION COMMANDS WILL BE PASSED THROUGH REMOTE SSH	
 	ssh root@$CHECKDOMAINIP -p22022 $CMDSUSPEND $COLLECTUSER \"FINANCEIRO\" 1
-
 fi
