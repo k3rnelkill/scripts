d7ddcf3f8e86cb737b67cbc61a24e168b315de4b
Thiago Marques <thiagomarquesdums@gmail.com>
2020-01-26 20:36:28 -0300

    Update acctsuspendedlist.sh

--- procedimentos/acctsuspendedlist.sh
+++ procedimentos/acctsuspendedlist.sh
@@ -50,7 +50,7 @@ then
             echo -e ""$YELLOW"HOSTNAME SERVER:"$DEFAULTCOLOR" $COLLECTHOSTNAME"
             echo -e ""$YELLOW"User cPanel.:"$DEFAULTCOLOR" $COLLECTUSER"	
             #SUSPENSION COMMANDS WILL BE PASSED THROUGH REMOTE SSH	
-            #ssh root@$CHECKDOMAINIP -p$SSHPORT $CMDSUSPEND $COLLECTUSER \"FINANCEIRO\" 1
+            ssh root@$CHECKDOMAINIP -p$SSHPORT $CMDSUSPEND $COLLECTUSER \"FINANCEIRO\" 1
             echo -e ""$BLUE"+++++++++++++++++++++++++++++++++++++++++++++++++++++++"$DEFAULTCOLOR""
         fi
     done
