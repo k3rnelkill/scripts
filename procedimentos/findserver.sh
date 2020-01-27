7cf4e8909d12ebd946bbd2160e374fa315d95f1b
Thiago Marques <thiagogardenajj@gmail.com>
2020-01-27 20:44:01 -0300

    Find and show IP/Hostname

--- procedimentos/findserver.sh
+++ procedimentos/findserver.sh
@@ -2,14 +2,14 @@
 
 ##########################################################################################################################################
 #                                                                                                                                        #
-# Name: findserver.sh                                                                                                           	 #
+# Name: findserver.sh                                                                                                                    #
 #                                                                                                                                        #
 # Author: Thiago Marques (thiagomarquesdums@gmail.com)                                                                                   #
 # Date: 20/01/20                                                                                                                         #
 #                                                                                                                                        #
-# Description: script finds the server and connect.                                                                     		 #
+# Description: find and show hostname e IP server.                                                                                       #
 #                                                                                                                                        #
-# Use: findserver															 #
+# Use: findserver                                                                                                                        #
 #                                                                                                                                        #
 ##########################################################################################################################################
 
@@ -26,33 +26,23 @@ SSHPORT="22022"
 #VARIABLE USED TO STORE DOMAIN
 read -p "Enter the domain.: " DOMAIN
 #VARIABLE USED TO STORE CPANEL DNS CLUSTER
-CLUSTERNAMESERVER="CLUSTER DNS HERE"
-#SCRIPT TO SUSPENSION
-CMDSUSPEND="/scripts/suspendacct"
+CLUSTERNAMESERVER="CLUSTER DNS SERVER HERE"
 #CHECKDOMAIN IN SERVER
 CHECKDOMAINIP=$(dig A cpanel.$DOMAIN $CLUSTERNAMESERVER  | grep -w "cpanel.$DOMAIN" | tail -n1 | head -1 | awk '{print $5}')
 
 #IF VARIABLE $CHECKDOMAINIP IS NULL, DOMAIN is not found. IN CASE OF NON-NULL VARIABLE. ELSE DEIPT THIS SUSPENSION WILL BE EXECUTED...
 if [ -z $CHECKDOMAINIP ]
 then
-	echo -e ""$RED"Variable is empty - $DOMAIN not found"$DEFAULTCOLOR""
-	echo -e ""$RED"$DOMAIN not found in DNS PULL"$DEFAULTCOLOR""
+        echo -e ""$RED"Variable is empty - $DOMAIN not found"$DEFAULTCOLOR""
+        echo -e ""$RED"$DOMAIN not found in DNS PULL"$DEFAULTCOLOR""
 else
-	
-    #COLLECT USER IN CPANEL
-    COLLECTUSER=$(ssh root@$CHECKDOMAINIP -p$SSHPORT grep -w $DOMAIN /etc/trueuserdomains | awk '{print $2}')
+
     #COLLECT HOSTNAME
     COLLECTHOSTNAME=$(ssh root@$CHECKDOMAINIP -p$SSHPORT uname -n)
 
     #SHOW INFORMATION TO USER
     echo -e ""$GREEN"Show information"$DEFAULTCOLOR""
-    echo -e ""$YELLOW"Domain.:"$DEFAULTCOLOR" $DOMAIN"
     echo -e ""$YELLOW"IP Server.:"$DEFAULTCOLOR" $CHECKDOMAINIP"
     echo -e ""$YELLOW"HOSTNAME SERVER:"$DEFAULTCOLOR" $COLLECTHOSTNAME"
-    echo -e ""$YELLOW"User cPanel.:"$DEFAULTCOLOR" $COLLECTUSER"
-    echo -e ""$GREEN"CONNECTING IN SERVER ..."$DEFAULTCOLOR""
-    sleep 1
 
-    #CONNECTING IN SSH SERVER	
-    ssh root@$CHECKDOMAINIP -p$SSHPORT
 fi
