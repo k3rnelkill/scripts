79009389a751d01a8b57a82b5615618b485c3829
Thiago Marques <thiagogardenajj@gmail.com>
2020-01-27 20:25:15 -0300

    Find and connecting server

---
+++ procedimentos/findserver.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+##########################################################################################################################################
+#                                                                                                                                        #
+# Name: acctsuspended.sh                                                                                                           	 #
+#                                                                                                                                        #
+# Author: Thiago Marques (thiagomarquesdums@gmail.com)                                                                                   #
+# Date: 20/01/20                                                                                                                         #
+#                                                                                                                                        #
+# Description: script finds the server and suspends it.                                                                     		 #
+#                                                                                                                                        #
+# Use: acctsuspended															 #
+#                                                                                                                                        #
+##########################################################################################################################################
+
+clear
+
+#DEFINES COLORS
+DEFAULTCOLOR="\033[0m"
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+YELLOW="\033[1;33m"
+
+#DEFINE SSH PORT
+SSHPORT="22022"
+#VARIABLE USED TO STORE DOMAIN
+read -p "Enter the domain.: " DOMAIN
+#VARIABLE USED TO STORE CPANEL DNS CLUSTER
+CLUSTERNAMESERVER="CLUSTER DNS HERE"
+#SCRIPT TO SUSPENSION
+CMDSUSPEND="/scripts/suspendacct"
+#CHECKDOMAIN IN SERVER
+CHECKDOMAINIP=$(dig A cpanel.$DOMAIN $CLUSTERNAMESERVER  | grep -w "cpanel.$DOMAIN" | tail -n1 | head -1 | awk '{print $5}')
+
+#IF VARIABLE $CHECKDOMAINIP IS NULL, DOMAIN is not found. IN CASE OF NON-NULL VARIABLE. ELSE DEIPT THIS SUSPENSION WILL BE EXECUTED...
+if [ -z $CHECKDOMAINIP ]
+then
+	echo -e ""$RED"Variable is empty - $DOMAIN not found"$DEFAULTCOLOR""
+	echo -e ""$RED"$DOMAIN not found in DNS PULL"$DEFAULTCOLOR""
+else
+	
+    #COLLECT USER IN CPANEL
+    COLLECTUSER=$(ssh root@$CHECKDOMAINIP -p$SSHPORT grep -w $DOMAIN /etc/trueuserdomains | awk '{print $2}')
+    #COLLECT HOSTNAME
+    COLLECTHOSTNAME=$(ssh root@$CHECKDOMAINIP -p$SSHPORT uname -n)
+
+    #SHOW INFORMATION TO USER
+    echo -e ""$GREEN"Show information"$DEFAULTCOLOR""
+    echo -e ""$YELLOW"Domain.:"$DEFAULTCOLOR" $DOMAIN"
+    echo -e ""$YELLOW"IP Server.:"$DEFAULTCOLOR" $CHECKDOMAINIP"
+    echo -e ""$YELLOW"HOSTNAME SERVER:"$DEFAULTCOLOR" $COLLECTHOSTNAME"
+    echo -e ""$YELLOW"User cPanel.:"$DEFAULTCOLOR" $COLLECTUSER"
+    echo -e ""$GREEN"CONNECTING IN SERVER ..."$DEFAULTCOLOR""
+    sleep 1
+
+    #CONNECTION IN SSH SSH SERVER	
+    ssh root@$CHECKDOMAINIP -p$SSHPORT
+fi
