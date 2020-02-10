c0aac17bfe6f5eab8a467350c144ccf2d52b5d79
Thiago Marques <thiagomarquesdums@gmail.com>
2020-02-10 10:22:17 -0300

    Count e-mail account user

--- procedimentos/consumptionreport.sh
+++ procedimentos/consumptionreport.sh
@@ -12,14 +12,17 @@ echo $LANG
 
 grep -w -i "$DAYNOW" $FILELOGPROCESSAMENT | grep "$YEARNOW" | awk '{print $8}' | sed 's/(\|)//g' | sort | uniq -c | sort -nr | tee /tmp/high_processcount.txt
 
+echo "+++++++++++++++++++++++++++++++++++++++++"
+
 LANG=$LANGBKP
 #whmapi1 showbw searchtype=user search=alugue79
 for i in $(cat /tmp/high_processcount.txt | awk '{print $2}')
 do
         echo "Usuário: $i"
         #echo "Picos de processos: $(cat /tmp/high_processcount.txt | awk '{print $1}')"
-        echo "Picos de processos: $(cat /tmp/high_processcount.txt | grep $i | awk '{print $1}')"
-        echo "Banda: $(whmapi1 showbw searchtype=user search=$i | grep "totalbytes:" | sed "s/'//g" | awk '{print $2}')"
+        echo "Número de contas de E-mail.: $(uapi --user=$i Email list_pops | grep email | wc -l)"
+        echo "Número de Picos de processos: $(cat /tmp/high_processcount.txt | grep $i | awk '{print $1}')"
+        echo "Banda em Bytes: $(whmapi1 showbw searchtype=user search=$i | grep "totalbytes:" | sed "s/'//g" | awk '{print $2}')"
         echo "+++++++++++++++++++++++++++++++++++++++++"
 done
 
