c7be3443fcded85187e859f320d546f14a22c66f
Thiago Dantas <LATAM-EIG\tdantas@endusys2164.latam-eig.com>
2020-02-12 16:08:02 -0300

    ...

--- procedimentos/consumptionreport.sh
+++ procedimentos/consumptionreport.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
+LC_ALL=C
+
+#DEFINES COLORS
+DEFAULTCOLOR="\033[0m"
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+YELLOW="\033[1;33m"
+
 LANGBKP=$LANG
 LANG="en_US:en"
 FILELOGPROCESSAMENT="/opt/hgmods/hg_processcount.log"
 DAYNOW=$(/bin/date "+%b %d")
 YEARNOW=$(/bin/date "+%Y")
+FILEDOMAIN="/etc/trueuserdomains"
+TMPFILE="/tmp/sumsend.tmp"
 
 echo $DAYNOW
 echo $YEARNOW
 echo $LANG
 
-grep -w -i "$DAYNOW" $FILELOGPROCESSAMENT | grep "$YEARNOW" | grep "danger" | awk '{print $8}' | sed 's/(\|)//g' | sort | uniq -c | sort -nr | tee /tmp/high_processcount.txt
+grep -w -i "$DAYNOW" $FILELOGPROCESSAMENT | grep "$YEARNOW" | awk '{print $8}' | sed 's/(\|)//g' | sort | uniq -c | sort -nr | tee /tmp/high_processcount.txt
 
 echo "+++++++++++++++++++++++++++++++++++++++++"
 
 LANG=$LANGBKP
-#whmapi1 showbw searchtype=user search=alugue79
+
 for i in $(cat /tmp/high_processcount.txt | awk '{print $2}')
 do
-        ec "$i" -e | grep "User sent approximately" > /tmp/email_send.txt
-        echo "Usuário: $i"
-        #echo "Picos de processos: $(cat /tmp/high_processcount.txt | awk '{print $1}')"
-        echo "Número de contas de E-mail.: $(uapi --user=$i Email list_pops | grep email | wc -l)"
-        echo "Número de Picos de processos: $(cat /tmp/high_processcount.txt | grep $i | awk '{print $1}')"
-        echo "Banda em Bytes: $(whmapi1 showbw searchtype=user search=$i | grep "totalbytes:" | sed "s/'//g" | awk '{print $2}')"
-        echo "Qtd de e-mails enviados: $(cat /tmp/email_send.txt | awk '{print $4}')"
+        DOMAIN=$(grep -w $i /etc/trueuserdomains | awk -F\: '{print $1}')
+       
+        SENDERCOUNTER=$(/bin/ls -l /var/cpanel/email_send_limits/track/${DOMAIN}/ | grep ${DAYNOW}| awk '{print $5}' | egrep -v '(4096|36864)' | sed '/^$/d' > ${TMPFILE})
+        : '
+           INFORMATION ABOUNT VARIABLE COUNTER AND PASTE|BC COMMAND
+           -d, --delimiters=LISTA  reutiliza caracteres da LISTA em vez de tabulações
+           -s, --serial            cola um arquivo por vez em de todos em paralelo
+
+           bc this a basic calculator      
+        '
+        COUNTER=$(paste -sd+ ${TMPFILE} | bc)
+        echo "Usuário: ${i}"
+        echo "Domínio Principal: ${DOMAIN}"
+        echo "Número de contas de E-mail.: $(uapi --user=${i} Email list_pops | grep email | wc -l)"
+        echo "Qtd de e-mails enviados: ${COUNTER}"
+        echo "Número de Picos de processos: $(cat /tmp/high_processcount.txt | grep ${i} | awk '{print $1}')"
+        echo "Banda em Bytes: $(whmapi1 showbw searchtype=user search=${i} | grep "totalbytes:" | sed "s/'//g" | awk '{print $2}')"
         echo "+++++++++++++++++++++++++++++++++++++++++"
 done
 
